b69e7cdff05bd42f15d08600dc4437faa8db5870
Everton Fernandes Mateus <everton.fmateus@gmail.com>
2020-08-09 19:29:18 -0300

    add server project

---
+++ server/README.md
@@ -0,0 +1,13 @@
+# Funcionalidades
+
+## Conexões
+
+- Rota listar o total de conexões realizadas
+- Rota criar uma nova conexão (aluno com professor)
+
+## Aulas
+
+- Rota criar uma aula
+- Rotalistar listar aulas
+  - filtrar por materia dia da semana e horaio
+
---
+++ server/src/server.ts
@@ -0,0 +1,16 @@
+//server.ts
+
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+
+console.log('(server started! 127.0.0.1:3333)');
+app.listen(3333);
+
